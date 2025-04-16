b3346f93106a2cb8ce7606b87b3b629b4e1ea089
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-04-16 17:59:01 +0300

    fixed a typo in Home.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I am currently a learner in this realm of software development but you shall not be mistaken in addressing me as a progidy</p>
+    {image ? <img src={image} alt="I made this" /> : null}
+  </div>;
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   // update the JSX being returned!
   return <div id="home">
     <h1 style={{ color: "firebrick" }}>
-      {`Your name is ${name} and you live in ${city}.`};
+      {`${name} is a Web Developer from ${city}`}
     </h1>
   </div>;
 }
